0191dd52f5a9aed0ddb7447393af16d1f4b46022
ahorner54 <ahorner589@gmail.com>
2024-04-22 15:15:25 -0500

    Created superuser for site and fixed admin.py errors

--- ExpenseTracker/budget/admin.py
+++ ExpenseTracker/budget/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Semester, Income, Expense
 
 # Register your models here.
-admin.register(Semester)
-admin.register(Income)
-admin.register(Expense)
+admin.site.register(Semester)
+admin.site.register(Income)
+admin.site.register(Expense)
