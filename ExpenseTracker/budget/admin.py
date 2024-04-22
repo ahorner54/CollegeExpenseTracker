da483fe02bd6e49e10413bb57196f37dced7b124
ahorner54 <ahorner589@gmail.com>
2024-04-22 15:05:53 -0500

    Registered Semester, Income, and Expense in budget/admin.py

--- ExpenseTracker/budget/admin.py
+++ ExpenseTracker/budget/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from models import Semester, Income, Expense
 
 # Register your models here.
+admin.register(Semester)
+admin.register(Income)
+admin.register(Expense)
