274a8cf51579c0442494ab8d5b7e5a95d517828f
ahorner54 <ahorner589@gmail.com>
2024-04-27 13:36:26 -0500

    Added login_user view

--- ExpenseTracker/budget/views.py
+++ ExpenseTracker/budget/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request, 'budget/home.html', {})
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, 'Login failed, try again.')
+            return redirect('home')
+    else:
+        return render(request, 'budget/login.html', {})
