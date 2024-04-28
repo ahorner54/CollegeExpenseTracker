f2bfdd3b9a7efec5c7f06cb11f8c759651787963
ahorner54 <ahorner589@gmail.com>
2024-04-28 16:14:16 -0500

    Fixed ExpenseTracker register url

--- ExpenseTracker/ExpenseTracker/urls.py
+++ ExpenseTracker/ExpenseTracker/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('budget.urls')),
-    path('', include('registration.urls')),
+    path('register/', include('registration.urls')),
 ]
