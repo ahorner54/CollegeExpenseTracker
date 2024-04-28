264b6938f0defaed098934f80a9eefd1f021ab8d
ahorner54 <ahorner589@gmail.com>
2024-04-28 17:12:09 -0500

    Added semester_view.html content and changed dropdownlist

--- ExpenseTracker/budget/views.py
+++ ExpenseTracker/budget/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+from .models import Semester, Income, Expense
 
 # Create your views here.
 def home(request):
-    context={}
-    return render(request, 'budget/home.html', context=context)
+    return render(request, 'budget/home.html', {})
+
+def semester(request, pk):
+    if not(request.user.is_authenticated):
+        return redirect('home')
+    else:
+        user_pk = request.user.pk
+        semesters = Semester.objects.filter(student_id = user_pk)
+        context = {
+           'semester_list': semesters
+        }
+        return render(request, 'budget/semester_view.html', context=context)
 
 def login_user(request):
     if request.method == 'POST':
