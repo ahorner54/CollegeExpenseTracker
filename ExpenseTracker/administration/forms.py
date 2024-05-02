3739ca6d897b391095256f305154fb2aa4a81d7e
ahorner54 <ahorner589@gmail.com>
2024-05-02 15:51:58 -0500

    not finsihed yet

--- ExpenseTracker/administration/forms.py
+++ ExpenseTracker/administration/forms.py
@@ -13,6 +13,17 @@ class AddUserForm(forms.ModelForm):
     model = User
     fields = ['first_name', 'last_name', 'email', 'username', 'password', 'groups']
 
+  def __init__(self, *args, **kwargs):
+    super(AddUserForm, self).__init__(*args, **kwargs)
+
+    self.fields["first_name"].widget.attrs["class"] = "form-control"
+    self.fields["first_name"].widget.attrs['placeholder'] = "First Name"
+    self.fields["first_name"].label = ""
+
+    self.fields["groups"].widget.attrs["class"] = "form-control"
+    self.fields["groups"].widget.attrs['placeholder'] = "First Name"
+    self.fields["groups"].label = ""
+
 
 class AddAdminForm(forms.ModelForm):
   email = forms.CharField(required=True, label="", widget=forms.widgets.TextInput(attrs={"class": "form-control", "placeholder": "Email"}))
