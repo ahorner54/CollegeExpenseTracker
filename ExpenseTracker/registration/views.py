af0d922ee50c33df3534bdd655edb9b82f109663
ahorner54 <ahorner589@gmail.com>
2024-05-03 17:01:30 -0500

    added test users and admin and final styling

--- ExpenseTracker/registration/views.py
+++ ExpenseTracker/registration/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from .forms import SignUpForm
+from django.contrib.auth.models import Group
 
 # Create your views here.
 def register_user(request):
@@ -9,10 +10,11 @@ def register_user(request):
         form = SignUpForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            new_user = form.save()
             username = form.cleaned_data['username']
             password = form.cleaned_data['password1']
-
+            student_group = Group.objects.get(name='Student')
+            new_user.groups.add(student_group.id)
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('home')
